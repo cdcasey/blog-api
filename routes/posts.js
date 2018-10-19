ae183f8f8bce332e10931aebdd3200bf869905df
Chris Casey <cdcasey@gmail.com>
2018-10-18 23:26:00 -0500

    Remove unneeded import

--- routes/posts.js
+++ routes/posts.js
@@ -3,7 +3,6 @@
 const express = require('express');
 const router = express.Router();
 
-const knex = require('../db/knex');
 const postsModel = require('../models/posts');
 const postsDB = new postsModel('posts');
 
