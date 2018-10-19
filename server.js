ef2d4fedeea2d3d55b1b867a861112cc3b8b90bc
Chris Casey <cdcasey@gmail.com>
2018-10-18 20:56:33 -0500

    Added ability to delete from browser

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ server.disable('x-powered-by');
 
 server.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, DELETE');
     res.header(
         'Access-Control-Allow-Headers',
         'Origin, X-Requested-With, Content-Type, Accept'
